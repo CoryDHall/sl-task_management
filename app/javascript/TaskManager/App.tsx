e7786f577ea204763f019441d70775c33714ae9f
Cory Hall <corydantehall@gmail.com>
2024-05-28 18:29:42 -0400

    init setup

---
+++ app/javascript/TaskManager/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function App() {
+  return (
+    <main>
+      <h1>Task Manager</h1>
+    </main>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path    = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    chunkFormat: "module",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}
