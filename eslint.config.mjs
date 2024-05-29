ec95091e838f66f37a292b6eb5873afe19f174bc
Cory Hall <corydantehall@gmail.com>
2024-05-29 16:58:12 -0400

    add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,107 @@
+import eslint from '@eslint/js';
+import reactRec from 'eslint-plugin-react/configs/recommended.js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['node_modules', 'vendor', 'public', 'app/assets'] },
+  eslint.configs.recommended,
+  reactRec,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ['**/*.{mjs,js}', '**/*.ts', '**/*.tsx'],
+    rules: {
+      'indent': [
+        'warn',
+        2,
+        {},
+      ],
+      'object-curly-spacing': [
+        'warn',
+        'always',
+        { 'objectsInObjects': false },
+      ],
+      'object-curly-newline': [
+        'warn',
+        {
+          'ObjectExpression': { 'multiline': true },
+          'ObjectPattern': { 'multiline': true },
+          'ImportDeclaration': { 'minProperties': 2 },
+        },
+      ],
+      'object-property-newline': [
+        'warn',
+        { 'allowAllPropertiesOnSameLine': true },
+      ],
+      'comma-dangle': [
+        'warn',
+        {
+          'arrays': 'always-multiline',
+          'objects': 'always-multiline',
+          'imports': 'always-multiline',
+          'exports': 'always-multiline',
+        },
+      ],
+      'quotes': [
+        'warn',
+        'single',
+        {
+          'allowTemplateLiterals': true,
+          'avoidEscape': true,
+        },
+      ],
+      'prefer-template': [
+        'warn',
+      ],
+      'computed-property-spacing': [
+        'warn',
+      ],
+      'array-element-newline': [
+        'warn',
+        'consistent',
+      ],
+      'react/jsx-indent-props': [
+        'warn',
+        2,
+      ],
+      'react/jsx-max-props-per-line': [
+        'warn',
+        { 'when': 'multiline' },
+      ],
+      'react/jsx-sort-props': [
+        'warn',
+        {
+          'callbacksLast': true,
+          'noSortAlphabetically': true,
+          'shorthandFirst': true,
+        },
+      ],
+      'react/jsx-wrap-multilines': [
+        'warn',
+      ],
+      'react/jsx-tag-spacing': [
+        'warn',
+      ],
+      'react/self-closing-comp': [
+        'warn',
+      ],
+      'react/jsx-newline': [
+        'warn',
+        {
+          'prevent': true,
+          'allowMultilines': true,
+        },
+      ],
+      'react/jsx-first-prop-new-line': [
+        'warn',
+        'multiline-multiprop',
+      ],
+      'semi': [
+        'error',
+      ],
+    },
+  }
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path    = require("path")
-const webpack = require("webpack")
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* global require, module, __dirname */
+
+const path    = require('path');
+const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.ts",
-  },
+  mode: 'production',
+  devtool: 'source-map',
+  entry: { application: './app/javascript/application.ts' },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
+  resolve: { extensions: ['.tsx', '.ts', '.js'] },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+    chunkFormat: 'module',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+    new ESLintPlugin({
+      extensions: ['ts', 'tsx'],
+      configType: 'flat',
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+  ],
+};
